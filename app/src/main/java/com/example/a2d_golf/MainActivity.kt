31fc3fc08e8fb3900cb59bac5263c6d1521cac0b
DavvveN <davidnlssn.abc@gmail.com>
2023-12-19 22:06:49 +0100

    Display background and level design

--- app/src/main/java/com/example/a2d_golf/MainActivity.kt
+++ app/src/main/java/com/example/a2d_golf/MainActivity.kt
@@ -5,12 +5,16 @@ import androidx.compose.foundation.Image
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -36,13 +40,30 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun initGameView(modifier: Modifier = Modifier) {
+    Column {
+        Box {
+            Image(
+                painter = painterResource(id = R.drawable.background),
+                contentScale = ContentScale.FillBounds,
+                contentDescription = null,
+                modifier = modifier.fillMaxSize()
+            )
+        }
+    }
     Column{
-        Image(
-            painter = painterResource( id = R.drawable.background),
-            contentScale = ContentScale.FillBounds,
-            contentDescription = null,
-            modifier = modifier.fillMaxSize()
-        )
+        Box(
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+                .fillMaxWidth()
+        ){
+            Image(
+                painter = painterResource(id = R.drawable.firstmap),
+                contentScale = ContentScale.FillBounds,
+                contentDescription = null,
+                modifier = modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight())
+        }
     }
 }
 
